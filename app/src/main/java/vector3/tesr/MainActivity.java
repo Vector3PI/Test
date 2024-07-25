45ffb886dfa66c9aeca0f3ac178b1d7654927250
SlimeLimeDeveloler <slimelime00123@gmail.com>
2024-07-25 16:34:22 +0700

    load project to github

---
+++ app/src/main/java/vector3/tesr/MainActivity.java
@@ -0,0 +1,23 @@
+package vector3.tesr;
+
+import android.app.Activity;
+import android.os.Bundle;
+import vector3.tesr.databinding.ActivityMainBinding;
+
+public class MainActivity extends Activity {
+	
+	  private ActivityMainBinding binding;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+		    binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.binding = null;
+    }
+}
